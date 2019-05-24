ca0a7d2948ce89fa6f676f63e02fd28d16e9e6b9
doc-manhattan <d.mhtn.init@gmail.com>
2019-05-24 14:17:07 +1000

    layout wip

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React, { Component } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import GlobalStyle from "./GlobalStyle";
-import Flex, { FlexItem } from "styled-flex-component";
-import { Nav, Item, Segment } from "./Components/Nav";
-import Logo from "./Components/Logo";
+import Header from "./Components/Header";
+import Lorem from "react-lorem-component";
 
-const Content = styled.div`
-  width: 960px;
+const Canvas = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: fill-available;
+  width: 100%;
+  overflow: auto;
+`;
+
+const Divider = styled.div`
+  height: 1px;
+  width: 100%;
+  padding-bottom: 5px;
+  box-shadow: 0 4px 2px -2px #e0e0e0;
+  margin-top: ${props => props.offSetTop || 0};
+`;
+
+const Container = styled.div`
+  max-width: 960px;
+  width: fill-available;
   padding: 0 2rem 0 2rem;
 `;
 
-const Header = styled.div`
-  width: 100%;
-  display: flex;
-  box-shadow: 0 4px 2px -2px #f0f0f0;
-  justify-content: center;
-`;
-
-const Page = styled.div`
+const Content = styled.div`
   padding: 2rem 0 2rem 0;
 `;
+
+//  box-shadow: 0 4px 2px -2px #f0f0f0;
 
 class App extends Component {
   render() {
@@ -28,40 +40,17 @@ class App extends Component {
       <ThemeProvider theme={theme}>
         <div>
           <GlobalStyle />
-          <Flex center full column>
-            <Header>
+          <Canvas>
+            <Container>
+              <Header />
+            </Container>
+            <Divider offSetTop={"-7px"} />
+            <Container>
               <Content>
-                <Logo />
-                <Nav>
-                  <Segment>
-                    <Item first active>
-                      <a href="/">Product</a>
-                    </Item>
-                    <Item>
-                      <a href="/">Whitepaper</a>
-                    </Item>
-                    <Item>
-                      <a href="/">Roadmap</a>
-                    </Item>
-                    <Item last>
-                      <a href="/">Team</a>
-                    </Item>
-                  </Segment>
-                  <Segment right>
-                    <Item first>
-                      <a href="/">Community</a>
-                    </Item>
-                    <Item last>
-                      <a href="/">Github</a>
-                    </Item>
-                  </Segment>
-                </Nav>
+                <Lorem count={30} />
               </Content>
-            </Header>
-            <Page>
-              <Content>Hello</Content>
-            </Page>
-          </Flex>
+            </Container>
+          </Canvas>
         </div>
       </ThemeProvider>
     );
---
+++ src/Components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { Nav, Item, Segment } from "./Nav";
+import Logo from "./Logo";
+
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+export default () => {
+  return (
+    <Header>
+      <Logo />
+      <Nav>
+        <Segment>
+          <Item first active>
+            <a href="/">Product</a>
+          </Item>
+          <Item>
+            <a href="/">Whitepaper</a>
+          </Item>
+          <Item>
+            <a href="/">Roadmap</a>
+          </Item>
+          <Item last>
+            <a href="/">Team</a>
+          </Item>
+        </Segment>
+        <Segment right>
+          <Item first>
+            <a href="/">Community</a>
+          </Item>
+          <Item last>
+            <a href="/">Github</a>
+          </Item>
+        </Segment>
+      </Nav>
+    </Header>
+  );
+};
